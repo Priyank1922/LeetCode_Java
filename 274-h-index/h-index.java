7c963a7504c21062eb4527415251ca971d3d1ea4
Priyank Mehta <priyankmehta409@gmail.com>
2026-09-23 18:02:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (57.99%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int [] bucket = new int[n+1];
+        for(int i = 0 ; i < n ; i++){
+            int x = citations[i];
+
+            if(x >= n){
+                bucket[n]++;
+            }else{
+                bucket[x]++;
+            }
+        }
+        int count = 0;
+        for(int i = n ; i>= 0; i--){
+            count += bucket[i];
+            if(count >= i){
+                return i;
+            }
+        } 
+        return 0;
+    }
+}
