bc79e77fc6b6e5d9c4d6e8e11813ac346d543402
Priyank Mehta <priyankmehta409@gmail.com>
2026-09-24 17:19:46 +0530

    Time: 2 ms (99.89%) | Memory: 51.9 MB (59.55%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int candy(int[] ratings) {
+        if(ratings.length == 0 ){
+            return 0;
+        }
+        int res = 1 , up = 0 , down = 0 , peak = 0;
+
+        for(int i = 1 ; i < ratings.length ; i++){
+            int prev = ratings [i-1];
+            int curr = ratings[i];
+
+            if(prev < curr){
+                up++;
+                down = 0;
+                peak = up ;
+                res+=1+ up ;
+            }else if(prev == curr){
+                up = 0 ;
+                down = 0 ;
+                peak = 0 ;
+                res += 1;
+            }
+            else{
+                up = 0 ;
+                down ++;
+                res += 1+down;
+                if(peak >= down){
+                    res --;
+                }
+            }
+        }
+        return res;
+    }
+}
